b7e9a0edf5b90dea0267efbc990c095d302ad64d
Iacopo Marmorini <iacopo@develer.com>
2016-03-25 22:56:09 +0100

    fixup and improve firing from enemy 1
    * double concentric fire
    * better timing randomness
    * now the enemy always look at the player

--- Assets/Scripts/Enemy1Logic.cs
+++ Assets/Scripts/Enemy1Logic.cs
@@ -5,21 +5,40 @@ public class Enemy1Logic : MonoBehaviour {
 
 	public GameObject proiettile;
 
+	private float firingInterval;
+	private float tLastFire;
+
 	// Use this for initialization
 	void Start () {
-		transform.LookAt(GameObject.FindGameObjectWithTag("Player").transform);
+		tLastFire = Time.time;
+
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+
+		// Rotate the enemy towards the player
+		transform.LookAt(player.transform);
 	}
 
 	void Spara() {
-		GameObject go = (GameObject)Instantiate (proiettile, transform.position + (transform.forward * 1.0f), new Quaternion());
-		Rigidbody rb = go.GetComponent<Rigidbody> ();
-		rb.AddForce (transform.forward, ForceMode.Impulse);
+		GameObject b1 = (GameObject)Instantiate (proiettile, transform.position + (transform.forward + transform.right), new Quaternion());
+		GameObject b2 = (GameObject)Instantiate (proiettile, transform.position + (transform.forward - transform.right), new Quaternion());
+		Rigidbody rb1 = b1.GetComponent<Rigidbody> ();
+		Rigidbody rb2 = b2.GetComponent<Rigidbody> ();
+
+
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		Vector3 heading1 = player.transform.position - b1.transform.position;
+		Vector3 heading2 = player.transform.position - b2.transform.position;
+		rb1.AddForce (heading1, ForceMode.Impulse);
+		rb2.AddForce (heading2, ForceMode.Impulse);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Random.value < 0.01) {
+		if ((Time.time - tLastFire) > firingInterval) {
 			Spara ();
+
+			tLastFire = Time.time;
+			firingInterval = 0.5F + Random.value * 3;
 		}	
 	}
 }
