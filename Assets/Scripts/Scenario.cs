5746a1114c5adc7fcc3190b7848d27ed4f04be16
Lorenzo Berni <duplo@develer.com>
2016-03-25 18:09:54 +0100

    Add basic scenario

---
+++ Assets/Scripts/Scenario.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Scenario : MonoBehaviour {
+	public GameObject scenario;
+
+	private GameObject prev;
+	private GameObject current;
+	private GameObject next;
+
+	private int unit_depth;
+
+	// Use this for initialization
+	void Start () {
+		ScenarioUnit g = scenario.GetComponent<ScenarioUnit> ();
+		unit_depth = 2 * g.depth + 1;
+		Physics.IgnoreLayerCollision (8, 8);
+		prev = Inst (new Vector3(0, 0, -unit_depth));
+		current = Inst (new Vector3(0, 0, 0));
+		next = Inst (new Vector3(0, 0, unit_depth));
+	}
+
+	GameObject Inst (Vector3 position) {
+		GameObject go = (GameObject)Instantiate (scenario, position, new Quaternion ());
+		return go;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (prev.transform.position.z <  -unit_depth) {
+			GameObject tmp = prev;
+			prev = current;
+			current = next;
+			next = Inst (new Vector3 (0, 0, current.transform.position.z + unit_depth));
+			Destroy (tmp);
+		}
+	}
+}
---
+++ Assets/Scripts/ScenarioUnit.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScenarioUnit : MonoBehaviour {
+	public GameObject prefab;
+	public int width;
+	public int depth;
+	public float density;
+
+	// Use this for initialization
+	void Start () {
+		for (int x = -width; x <= width; ++x) {
+			for (int z = -depth; z <= depth; ++z) {
+				if (Random.value < density) {
+					Inst (x, z);
+				}
+			}
+		}
+	}
+
+	void Inst (int x, int z) {
+		GameObject go = (GameObject) Instantiate (prefab, new Vector3(x, Random.value * 2, z), new Quaternion());
+		go.transform.SetParent (this.transform, false);
+		float size = Random.value;
+		float gs = Random.value;
+		go.transform.localScale = new Vector3 (size, size, size);
+		Material material = go.GetComponent<Renderer>().material;
+		material.color = new Color(gs, gs, gs);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
