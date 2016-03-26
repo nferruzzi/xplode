e0e349f424bac6b9f2806c1667d999b58c8128c5
Nicola Ferruzzi <nicola.ferruzzi@gmail.com>
2016-03-26 22:41:28 +0100

    Spawnwer: calc spawning points with a margin outside the boundary

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,18 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [ExecuteInEditMode]
 public class Spawner : MonoBehaviour {
 
-	public float gameLevel = 5.0f;
+	public float gameYLevel = 5.0f;
 	public float sideSubdivision = 5.0f;
+	public float spawnPointMargin = 4.0f;
+	public List<Vector3> spawnLocationsTop;
+	public List<Vector3> spawnLocationsLeft;
+	public List<Vector3> spawnLocationsRight;
+	public Vector3 nBoundaryUp, nBoundaryLef, nBoundaryRight;
+
 	Vector3 bl, br, tl, tr;
 
 	void Start () {
-		CalcFrustumPlane ();
+		CalcAll ();
 	}
 
-	void CalcFrustumPlane () {
+	void CalcAll() {
+		CalcFrustumPlane (gameYLevel, ref bl, ref br, ref tr, ref tl);
+		CalcSpawningPoints ();
+	}
+
+	void CalcFrustumPlane (float yLevel, ref Vector3 lbl, ref Vector3 lbr, ref Vector3 ltr, ref Vector3 ltl) {
 		Camera ca = Camera.main;
 
 		Ray bottomLeftRay = ca.ViewportPointToRay(new Vector3(0, 0, 0));
@@ -20,7 +32,7 @@ public class Spawner : MonoBehaviour {
 		Ray topRightRay = ca.ViewportPointToRay(new Vector3(1, 1, 0));
 		Ray bottomRightRay = ca.ViewportPointToRay(new Vector3(1, 0, 0));
 
-		Plane pl = new Plane (Vector3.up, new Vector3 (0, gameLevel, 0));
+		Plane pl = new Plane (Vector3.up, new Vector3 (0, yLevel, 0));
 
 		float bottomLeftDistance;
 		float topLeftDistance;
@@ -28,19 +40,55 @@ public class Spawner : MonoBehaviour {
 		float topRightDistance;
 
 		if (pl.Raycast (bottomLeftRay, out bottomLeftDistance)) {
-			bl = bottomLeftRay.GetPoint(bottomLeftDistance);
+			lbl = bottomLeftRay.GetPoint(bottomLeftDistance);
 
 			if (pl.Raycast (topLeftRay, out topLeftDistance)) {
-				tl = topLeftRay.GetPoint(topLeftDistance);
+				ltl = topLeftRay.GetPoint(topLeftDistance);
 
 				if (pl.Raycast (bottomRightRay, out bottomRightDistance)) {
-					br = bottomRightRay.GetPoint(bottomRightDistance);
+					lbr = bottomRightRay.GetPoint(bottomRightDistance);
 
 					if (pl.Raycast (topRightRay, out topRightDistance)) {
-						tr = topRightRay.GetPoint(topRightDistance);
+						ltr = topRightRay.GetPoint(topRightDistance);
 					}
 				}
 			}
+		}
+	}
+
+	void CalcSpawningPoints() {
+//		Vector3 lbl = new Vector3(), lbr = new Vector3(), ltl = new Vector3(), ltr = new Vector3();
+//		CalcFrustumPlane (gameYLevel - 10.0f, ref lbl, ref lbr, ref ltr, ref ltl);
+
+		float sud = 1.0f / sideSubdivision;
+
+		Vector3 up = (tr + tl) * 0.5f;
+		Vector3 left = (tl + bl) * 0.5f;
+		Vector3 right = (tr + br) * 0.5f;
+		Vector3 center = (tr + tl + br + bl) * 0.25f;
+		nBoundaryUp = (up - center).normalized;
+		nBoundaryLef = (left - center).normalized;
+		nBoundaryRight = (right - center).normalized;
+
+		spawnLocationsTop = new List<Vector3> ();
+		for (float t = 0.0f; t <= 1.0f; t += sud) {
+			Vector3 point = Vector3.Lerp (tl, tr, t);
+			point += nBoundaryUp * spawnPointMargin;
+			spawnLocationsTop.Add (point);
+		}
+
+		spawnLocationsLeft = new List<Vector3> ();
+		for (float t = sud; t <= 1.0f; t += sud) {
+			Vector3 point = Vector3.Lerp (tl, bl, t);
+			point += nBoundaryLef * spawnPointMargin;
+			spawnLocationsLeft.Add (point);
+		}
+
+		spawnLocationsRight = new List<Vector3> ();
+		for (float t = sud; t <= 1.0f; t += sud) {
+			Vector3 point = Vector3.Lerp (tr, br, t);
+			point += nBoundaryRight * spawnPointMargin;
+			spawnLocationsRight.Add (point);
 		}
 	}
 
@@ -48,7 +96,7 @@ public class Spawner : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		CalcFrustumPlane ();
+		CalcAll ();
 	}
 
 	void OnDrawGizmos() {
@@ -57,6 +105,17 @@ public class Spawner : MonoBehaviour {
 		Gizmos.DrawLine (br, tr);
 		Gizmos.DrawLine (tr, tl);
 		Gizmos.DrawLine (tl, bl);
+
+		Gizmos.color = Color.red;
+		foreach (Vector3 point in spawnLocationsTop) {
+			Gizmos.DrawWireSphere (point, 0.2f);
+		}
+		foreach (Vector3 point in spawnLocationsLeft) {
+			Gizmos.DrawWireSphere (point, 0.2f);
+		}
+		foreach (Vector3 point in spawnLocationsRight) {
+			Gizmos.DrawWireSphere (point, 0.2f);
+		}
 	}
 
 }
