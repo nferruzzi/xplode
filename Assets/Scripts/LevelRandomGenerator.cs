3e5e7e4c00c454834e405b0be87e797443d3c31b
Nicola Ferruzzi <nicola.ferruzzi@gmail.com>
2016-03-28 01:27:29 +0200

    Level random generator: add stupid random level generator; spawn enemies every x sec

---
+++ Assets/Scripts/LevelRandomGenerator.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SpawnableEnemy {
+	public GameObject enemey;
+	public int minPerSpawn = 1;
+	public int maxPerSpawn = 2;
+	public float delay = 1.0f;
+}
+
+public class LevelRandomGenerator : MonoBehaviour
+{
+	public float timeStart = 2.0f;
+	public float timeEnd = 100.0f;
+
+	public float spawningFrequency = 1.0f;
+	public List<SpawnableEnemy> spawnableEnemies;
+
+
+	void Start () {	
+		StartGeneration ();
+	}
+
+	void StartGeneration () {
+		InvokeRepeating ("Spawn", timeStart, spawningFrequency);
+	}
+
+	void Spawn () {
+		if (spawnableEnemies.Count == 0)
+			return;
+		
+		int index = Random.Range (0, spawnableEnemies.Count);
+		SpawnableEnemy se = spawnableEnemies [index];
+
+		int n = Random.Range (se.minPerSpawn, se.maxPerSpawn);
+		while (n > 0) {
+			StartCoroutine (SpawnAfter (se.enemey, Random.value * se.delay));
+			--n;
+		}
+	}
+
+	IEnumerator SpawnAfter(GameObject go, float delay) {
+		yield return new WaitForSeconds(delay);
+		Instantiate (go);
+	}
+	
+	void Update () {
+	}
+}
+
