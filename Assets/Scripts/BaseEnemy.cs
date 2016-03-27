f560de566ac4ba4564d3bd48ca2a82aaa9661654
Nicola Ferruzzi <nicola.ferruzzi@gmail.com>
2016-03-28 01:29:37 +0200

    Base enemy: use Spawner to get starting location

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -4,13 +4,18 @@ using System.Collections;
 public class BaseEnemy : MonoBehaviour {
 
 	public float energy = 10.0f;
+	Spawner spawner;
 
-	// Use this for initialization
 	void Start () {
-		
+		spawner = Spawner.GetInstance ();
+		int index = Random.Range (0, spawner.spawnLocationsTop.Count);
+		transform.position = spawner.spawnLocationsTop [index];
+		transform.rotation = Quaternion.LookRotation(-spawner.nBoundaryUp, Vector3.up);
+		Destroy (this.gameObject, 5.0f);
 	}
 	
 	void Update () {	
+		transform.position += -spawner.nBoundaryUp * Time.deltaTime * 5.0f;
 	}
 
 	void OnCollisionEnter(Collision collision) {
