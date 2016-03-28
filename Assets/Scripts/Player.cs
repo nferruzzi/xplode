814becfbeaa2f779479673088eb40fef4c1b23b7
Nicola Ferruzzi <nicola.ferruzzi@gmail.com>
2016-03-28 16:21:41 +0200

    Player: restart on death

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,23 +3,43 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	public float energy = 10.0f;
+	Vector3 restart;
 
 	void Start () {
+		restart = transform.position;
 	}
-	
+
+	void Respawn() {
+		transform.position = restart;
+		energy = 10;
+		gameObject.SetActive (true);
+	}
+
+	void Die () {
+		WeaponController wc = gameObject.GetComponent<WeaponController> ();
+		wc.FireUp ();
+		Invoke ("Respawn", 3.0f);
+		gameObject.SetActive (false);
+	}
+
 	void Update () {
 	}
 
 	void OnCollisionEnter(Collision collision) {
 		Proiettile pro = collision.gameObject.GetComponentInChildren<Proiettile> ();
-		energy -= pro.damage;
+		if (pro != null) {
+			energy -= pro.damage;
 
-		foreach (ContactPoint contact in collision.contacts) {
-			pro.ShowExplosionAt (contact.point, contact.normal);
+			foreach (ContactPoint contact in collision.contacts) {
+				pro.ShowExplosionAt (contact.point, contact.normal);
+			}
+
+			if (energy <= 0.0f) {
+				Die ();
+			}
+		} else {
+			// Insta death
+			Die ();
 		}
-
-		if (energy <= 0.0f) {
-			Destroy (this.gameObject);
-		}	
 	}
 }
