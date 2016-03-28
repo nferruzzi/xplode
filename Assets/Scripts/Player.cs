187402bfc9b11e18775675bc072d8bb3bddabcf9
Nicola Ferruzzi <nicola.ferruzzi@gmail.com>
2016-03-28 11:38:38 +0200

    Player: gets damaged and destroyed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,24 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+	public float energy = 10.0f;
 
-	// Use this for initialization
 	void Start () {
 	}
 	
-	// Update is called once per frame
 	void Update () {
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		Proiettile pro = collision.gameObject.GetComponentInChildren<Proiettile> ();
+		energy -= pro.damage;
+
+		foreach (ContactPoint contact in collision.contacts) {
+			pro.ShowExplosionAt (contact.point, contact.normal);
+		}
+
+		if (energy <= 0.0f) {
+			Destroy (this.gameObject);
+		}	
 	}
 }
