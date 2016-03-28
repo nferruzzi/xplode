cfc3cdace403b5d99274e6122f0ce8cbaaf00723
Nicola Ferruzzi <nicola.ferruzzi@gmail.com>
2016-03-28 11:37:54 +0200

    Base Enemy: add gun

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -4,18 +4,33 @@ using System.Collections;
 public class BaseEnemy : MonoBehaviour {
 
 	public float energy = 10.0f;
+	public float fireAfterMin = 1.0f;
+	public float fireAfterMax = 5.0f;
+
+	bool spara = false;
 	Spawner spawner;
+	WeaponGun gun;
 
 	void Start () {
+		gun = GetComponent<WeaponGun> ();
 		spawner = Spawner.GetInstance ();
 		int index = Random.Range (0, spawner.spawnLocationsTop.Count);
 		transform.position = spawner.spawnLocationsTop [index];
 		transform.rotation = Quaternion.LookRotation(-spawner.nBoundaryUp, Vector3.up);
 		Destroy (this.gameObject, 5.0f);
+		Invoke ("Spara", fireAfterMin + Random.value * (fireAfterMax - fireAfterMin));
 	}
 	
 	void Update () {	
 		transform.position += -spawner.nBoundaryUp * Time.deltaTime * 5.0f;
+
+		if (spara) {
+			gun.FireDown (gameObject, transform.forward * 1.2f);
+		}
+	}
+
+	void Spara () {
+		spara = true;
 	}
 
 	void OnCollisionEnter(Collision collision) {
